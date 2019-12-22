097addc0fc76cc42b19c9944cc1ab4621e564436
Penny <penny880514@gmail.com>
2019-12-23 01:18:52 +0800

    admin add doPost

--- WEB-INF/classes/ncu/im3069/demo/app/AdminHelper.java
+++ WEB-INF/classes/ncu/im3069/demo/app/AdminHelper.java
@@ -51,6 +51,72 @@ public class AdminHelper {
     }
     
     /**
+     * 建立該名管理員至資料庫
+     *
+     */
+    public JSONObject create(Admin a) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "INSERT INTO `sa_sharesport`.`admins`(`name`, `email`, `password`, `modified`, `created``)"
+                    + " VALUES(?, ?, ?, ?, ?)";
+            
+            /** 取得所需之參數 */
+            String name = a.getName();
+            String email = a.getEmail();
+            String password = a.getPassword();
+         
+            
+    
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, name);
+            pres.setString(2, email);
+            pres.setString(3, password);
+            pres.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            pres.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+            
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
+    }
+    
+    /**
      * 透過管理員編號（ID）刪除管理員
      *
      * @param id 管理員編號
